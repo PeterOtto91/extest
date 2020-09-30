db386882589da659c04854b822f89c4a33662ba7
PeterOtto91 <peter.otto@telenet.be>
2020-09-30 14:11:37 +0200

    exA6

---
+++ exA6/src/test/java/TestPerson.java
@@ -0,0 +1,49 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.Assert.assertThat;
+
+public class TestPerson {
+
+    private Person person1;
+
+    @Before
+    public void setUp() {
+
+        person1 = new Person(1, "Wim", "Gerets", LocalDate.of(1978, 5, 20));
+
+    }
+
+    @Test
+    public void ageofPersonFromBirthDateShouldBe42() {
+
+        // arrange
+        int result = 42;
+
+        //act
+        int ageNow = person1.calculateAge();
+
+        //assert
+        assertThat(result, equalTo(42));
+
+    }
+
+    @Test
+    public void toStringSenteceStartsWithPerson() {
+
+        // arrange
+
+        //act
+        String personString = person1.toString();
+
+        //assert
+        assertThat(personString, startsWith("Person"));
+
+
+    }
+
+}
